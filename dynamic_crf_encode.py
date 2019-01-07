70e370f2dd3172191efdc28c335313b11bb6982d
bingyu lu <bingyu.lu@hotstar.com>
2019-01-07 16:17:50 +0800

    use video_height instead of resolution

--- dynamic_crf_encode.py
+++ dynamic_crf_encode.py
@@ -8,10 +8,10 @@ import argparse
 common_settings = '-preset slow -b-pyramid normal -bf 3 -b_strategy 2 -err_detect compliant -mbtree 1 -tune film'
 variants_path = './data/variants.json'
 
-def get_video_profile_and_level(resolution):
+def get_video_profile_and_level(vheight):
     profile = None
     level = None
-
+    resolution = ''
     with open(variants_path) as file_variants:
         variants_list = json.load(file_variants)
         for variant in variants_list:
@@ -22,11 +22,12 @@ def get_video_profile_and_level(resolution):
                 video_height = video_item.get('height')
 
                 if video_width is not None and video_height is not None:
-                    if resolution == '{}x{}'.format(video_width, video_height):
+                    if str(vheight) == str(video_height):
                         profile  = video_item.get('profile')
                         level = video_item.get('level')
-                        break;
-    return profile, level
+                        resolution = "{}x{}".format(video_width, vheight)
+                        break
+    return profile, level, resolution
 
 def print_usage():
     print("usage: {cmd} input_video n_split [--start-offset start_offset] [--split-len split_len]".format(cmd=sys.argv[0]))
@@ -35,13 +36,13 @@ def get_duration(input_video):
     ffprobe_cmd = "ffprobe -v error -select_streams v:0 -show_entries stream=duration -of default=noprint_wrappers=1:nokey=1 {input_video}".format(input_video=input_video)
     return float(subprocess.check_output(ffprobe_cmd, shell=True).strip())
 
-def encode_segment(input_video, output_video, dst_res, start_time, end_time, crf, need_deinterlacing):
+def encode_segment(input_video, output_video, vheight, start_time, end_time, crf, need_deinterlacing):
     other_filter = ''
 
     if need_deinterlacing:
         other_filter += 'yadif,'
 
-    v_profilie, v_level = get_video_profile_and_level(dst_res)
+    v_profilie, v_level, resolution = get_video_profile_and_level(vheight)
 
     ffmpeg_cmd = 'ffmpeg -hide_banner -ss {start_time} -to {end_time} -i {input_video} -filter_complex "[0:v]{other_filter}scale={dst_res}[vout]" \
 -map [vout] -an -c:v libx264 {common_settings} -profile:v {v_profilie} -level {v_level} -crf {crf} -x264opts me=umh:merange=32:subme=10 -pix_fmt yuv420p -color_range tv -colorspace bt709 -color_trc bt709 -color_primaries bt709  -f mp4 {output_video} -y'.format(
@@ -52,10 +53,10 @@ def encode_segment(input_video, output_video, dst_res, start_time, end_time, crf
                 other_filter=other_filter,
                 v_profilie=v_profilie,
                 v_level=v_level,
-                dst_res=dst_res,
+                dst_res=resolution,
                 crf=crf,
                 output_video=output_video,
-            );
+            )
     print(ffmpeg_cmd)
     subprocess.call(ffmpeg_cmd, shell=True)
 
@@ -72,12 +73,12 @@ def do_merge(segment_list, output_video):
         ffmpeg_cmd = 'ffmpeg -hide_banner -f concat -safe 0 -i {segment_list_file} -c copy {output_video} -y'.format(
                 segment_list_file=f.name,
                 output_video=output_video,
-            );
+            )
         print(ffmpeg_cmd)
         subprocess.call(ffmpeg_cmd, shell=True)
         do_clean(segment_list)
 
-def encode_final(input_video, output_video, dst_res, seg_start_list, seg_duration_list, seg_crf_list, need_deinterlacing):
+def encode_final(input_video, output_video, vheight, seg_start_list, seg_duration_list, seg_crf_list, need_deinterlacing):
     assert len(seg_duration_list), "Length of seg_duration_list and seg_start_list are not equal."
     assert len(seg_crf_list), "Length of seg_crf_list and seg_start_list are not equal."
 
@@ -90,7 +91,7 @@ def encode_final(input_video, output_video, dst_res, seg_start_list, seg_duratio
         start_time = seg_start_list[seg_idx]
         end_time = start_time + seg_duration_list[seg_idx]
         seg_name = seg_format % seg_idx
-        encode_segment(input_video, seg_name, dst_res, start_time, end_time, seg_crf_list[seg_idx], need_deinterlacing)
+        encode_segment(input_video, seg_name, vheight, start_time, end_time, seg_crf_list[seg_idx], need_deinterlacing)
         segment_list.append(seg_name)
         
     do_merge(segment_list, output_video)
@@ -109,7 +110,7 @@ def get_segment_list(input_video):
         seg_crf_list.append(30 - seg_idx)
 
     return seg_start_list, seg_duration_list, seg_crf_list
-    
+
 if __name__ == '__main__':
     if len(sys.argv) < 1:
         print_usage()
@@ -117,9 +118,9 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Dynamic CRF encoding')
     parser.add_argument('input_video', help='Input video file path.')
     parser.add_argument('output_video', help='Output video file path.')
-    parser.add_argument('output_res', help='Output video resolution, in widthxheight format.')
+    parser.add_argument('video_height', help='Output video height.')
 
     args = parser.parse_args()
     seg_start_list, seg_duration_list, seg_crf_list = get_segment_list(args.input_video)
-    
+
     encode_final(args.input_video, args.output_video, args.output_res, seg_start_list, seg_duration_list, seg_crf_list, False)
